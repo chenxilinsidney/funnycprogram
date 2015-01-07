945a6b5a05cd42f98269d137d4203ea284aa70ba
Sidney Chen <chenxilinsidney@gmail.com>
2015-01-07 19:21:45 +0800

    program: odd-even sort

---
+++ introduction_to_algorithms/sort/sort_by_odd_even.c
@@ -0,0 +1,101 @@
+/**
+ * @file sort_by_odd_even.c
+ * @brief sort sequence by odd-even sort method.
+ * @author chenxilinsidney
+ * @version 1.0
+ * @date 2015-01-07
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+// #define NDEBUG
+#include <assert.h>
+
+#include "memory.h"
+// #define NDBG_PRINT
+#include "debug_print.h"
+
+typedef int TYPE;
+
+#define MAX_COUNT      100000
+TYPE array[MAX_COUNT] = {0};
+
+void odd_even_sort(TYPE* array, TYPE count)
+{
+    TYPE i;
+    TYPE flag_repeat = 1;
+    TYPE current_count = count - 1;
+    /// repeated until no swaps
+    while (flag_repeat) {
+        flag_repeat = 0;
+        /// pass by odd
+        for (i = 1; i < current_count; i += 2) {
+            /// compares adjacent items and swaps them if in wrong order.
+            if (array[i] > array[i + 1]) {
+                TYPE temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                flag_repeat = i;
+            }
+        }
+        for (i = 0; i < current_count; i += 2) {
+            /// compares adjacent items and swaps them if in wrong order.
+            if (array[i] > array[i + 1]) {
+                TYPE temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                flag_repeat = i;
+            }
+        }
+    }
+}
+
+int main(void) {
+    /// read data from file
+    FILE* fr = fopen("random_data.txt", "r");
+    if(fr == NULL) {
+        perror("random_data.txt");
+        DEBUG_PRINT_STATE;
+        fflush(stdout);
+        assert(0);
+        exit(EXIT_FAILURE);
+    }
+    /// read data to array
+    TYPE count = 0;
+    while(count < MAX_COUNT && fscanf(fr, "%u\n", array + count) == 1) {
+        ++count;
+    }
+    /// close read data file
+    if(fclose(fr) != 0) {
+        perror("random_data.txt");
+        DEBUG_PRINT_STATE;
+        fflush(stdout);
+        assert(0);
+        exit(EXIT_FAILURE);
+    }
+    /// sort data
+    odd_even_sort(array, count);
+    /// set write data file
+    FILE* fw = fopen("sorted_data.txt","w");
+    if(fw == NULL) {
+        perror("sorted_data.txt");
+        DEBUG_PRINT_STATE;
+        fflush(stdout);
+        assert(0);
+        exit(EXIT_FAILURE);
+    }
+    /// write sorted data to file
+    TYPE i;
+    for(i = 0; i < count; i++) {
+        fprintf(fw, "%u\n", array[i]);
+    }
+    /// close write data file
+    if(fclose(fw) != 0) {
+        perror("sorted_data.txt");
+        DEBUG_PRINT_STATE;
+        fflush(stdout);
+        assert(0);
+        exit(EXIT_FAILURE);
+    }
+    return EXIT_SUCCESS;
+}
