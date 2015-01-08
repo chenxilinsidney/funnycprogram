b05200853cbfd3530fc0658a7d3ac9b3464f1f9a
Sidney Chen <chenxilinsidney@gmail.com>
2015-01-08 22:32:37 +0800

    new program: quick sort

---
+++ introduction_to_algorithms/sort/sort_by_quick.c
@@ -0,0 +1,75 @@
+/**
+ * @file sort_by_quick.c
+ * @brief sort sequence by quicksort method.
+ * @author chenxilinsidney
+ * @version 1.0
+ * @date 2015-01-07
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+// #define NDEBUG
+#include <assert.h>
+
+#include "memory.h"
+// #define NDBG_PRINT
+#include "debug_print.h"
+
+typedef int TYPE;
+
+#define MAX_COUNT      100000
+TYPE array[MAX_COUNT] = {0};
+
+void quick_sort(TYPE* array, TYPE count)
+{
+}
+
+int main(void) {
+    /// read data from file
+    FILE* fr = fopen("random_data.txt", "r");
+    if(fr == NULL) {
+        perror("random_data.txt");
+        DEBUG_PRINT_STATE;
+        fflush(stdout);
+        assert(0);
+        exit(EXIT_FAILURE);
+    }
+    /// read data to array
+    TYPE count = 0;
+    while(count < MAX_COUNT && fscanf(fr, "%u\n", array + count) == 1) {
+        ++count;
+    }
+    /// close read data file
+    if(fclose(fr) != 0) {
+        perror("random_data.txt");
+        DEBUG_PRINT_STATE;
+        fflush(stdout);
+        assert(0);
+        exit(EXIT_FAILURE);
+    }
+    /// sort data
+    quick_sort(array, count);
+    /// set write data file
+    FILE* fw = fopen("sorted_data.txt","w");
+    if(fw == NULL) {
+        perror("sorted_data.txt");
+        DEBUG_PRINT_STATE;
+        fflush(stdout);
+        assert(0);
+        exit(EXIT_FAILURE);
+    }
+    /// write sorted data to file
+    TYPE i;
+    for(i = 0; i < count; i++) {
+        fprintf(fw, "%u\n", array[i]);
+    }
+    /// close write data file
+    if(fclose(fw) != 0) {
+        perror("sorted_data.txt");
+        DEBUG_PRINT_STATE;
+        fflush(stdout);
+        assert(0);
+        exit(EXIT_FAILURE);
+    }
+    return EXIT_SUCCESS;
+}
