2022af59a60fe88567f753cf1aa1e15e0bea03fb
Sidney Chen <chenxilinsidney@gmail.com>
2015-01-09 11:11:31 +0800

    first edition: Quicksort"

--- introduction_to_algorithms/sort/sort_by_quick.c
+++ introduction_to_algorithms/sort/sort_by_quick.c
@@ -20,8 +20,54 @@ typedef int TYPE;
 #define MAX_COUNT      100000
 TYPE array[MAX_COUNT] = {0};
 
-void quick_sort(TYPE* array, TYPE count)
+/**
+ * @brief select the last element as a pivoit.
+ * Reorder the array so that all elements with values less than the pivot
+ * come before the pivot, while all elements with values greater than the pivot
+ * come after it (equal values can go either way). After this partitioning, the
+ * pivot is in its final position. 
+ *
+ * @param[in,out]  array          input and output array
+ * @param[in]      index_begin    the begin index of the array(included)
+ * @param[in]      index_end      the end index of the array(included)
+ *
+ * @return the position of the pivot(index from the array)
+ */
+TYPE partition(TYPE* array, TYPE index_begin, TYPE index_end)
 {
+    /// select last element of the array as pivot
+    TYPE pivot = array[index_end];
+    /// index of the elments that not greater than pivot
+    TYPE i = index_begin - 1;
+    TYPE j;
+    /// check array's elment one by one
+    for (j = index_begin; j < index_end; j++) {
+        if (array[j] <= pivot) {
+            /// save the elements not greater than pivot to left index of i.
+            i++;
+            TYPE temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+    }
+    /// set the pivot to the right position
+    array[index_end] = array[++i];
+    array[i] = pivot;
+    /// return the position of the pivot
+    return i;
+}
+
+void quick_sort(TYPE* array, TYPE index_begin, TYPE index_end)
+{
+    /// sort only under the index_begin < index_end condition
+    if ( index_begin < index_end) {
+        /// exchange elements to the pivot position by partition function
+        TYPE index_pivot = partition(array, index_begin, index_end);
+        /// sort the array before the pivot position
+        quick_sort(array, index_begin, index_pivot - 1);
+        /// sort the array after the pivot position
+        quick_sort(array, index_pivot + 1, index_end);
+    }
 }
 
 int main(void) {
@@ -48,7 +94,7 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
     /// sort data
-    quick_sort(array, count);
+    quick_sort(array, 0, count - 1);
     /// set write data file
     FILE* fw = fopen("sorted_data.txt","w");
     if(fw == NULL) {
