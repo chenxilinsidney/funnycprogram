181ce119a1f7301eb247d063ce29689b80e6b94d
Sidney Chen <chenxilinsidney@gmail.com>
2015-01-11 14:20:11 +0800

    program: shell sort

--- introduction_to_algorithms/sort/sort_by_shell.c
+++ introduction_to_algorithms/sort/sort_by_shell.c
@@ -1,6 +1,6 @@
 /**
- * @file sort_by_insert.c
- * @brief sort sequence by insertion sort method.
+ * @file sort_by_shell.c
+ * @brief sort sequence by shell sort method.
  * @author chenxilinsidney
  * @version 1.0
  * @date 2015-01-07
@@ -20,21 +20,27 @@ typedef int TYPE;
 #define MAX_COUNT      100000
 TYPE array[MAX_COUNT] = {0};
 
-void insert_sort(TYPE* array, TYPE count)
+void shell_sort(TYPE* array, TYPE count)
 {
-    TYPE i,j, key;
-    /// check the other elements after the head element.
-    for (i = 1; i < count; i++) {
-        /// save the element temp
-        key = array[i];
-        /// move the elements one by one.
-        j = i;
-        while (j > 0 && key < array[j - 1]) {
-            array[j] = array[j - 1];
-            j--;
+    /// set gap sequence
+    TYPE gaps[8] = {701, 301, 132, 57, 23, 10, 4, 1};
+    TYPE i, j, k, key, gap;
+    for (k = 0; k < 8; k++) {
+        /// get current gap
+        gap = gaps[k];
+        /// check the other elements after the first gap element.
+        for (i = gap; i < count; i++) {
+            /// save the element temp
+            key = array[i];
+            /// move the elements one by one.
+            j = i;
+            while (j >= gap && key < array[j - gap]) {
+                array[j] = array[j - gap];
+                j -= gap;
+            }
+            /// insert the element
+            array[j] = key;
         }
-        /// insert the element
-        array[j] = key;
     }
 }
 
@@ -62,7 +68,7 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
     /// sort data
-    insert_sort(array, count);
+    shell_sort(array, count);
     /// set write data file
     FILE* fw = fopen("sorted_data.txt","w");
     if(fw == NULL) {
