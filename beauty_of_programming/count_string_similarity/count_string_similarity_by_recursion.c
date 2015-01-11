fb650d58a27a53b6e04c6b59b99b70865d7faad4
Sidney Chen <chenxilinsidney@gmail.com>
2015-01-11 15:59:04 +0800

    comments:count string similarity

--- beauty_of_programming/count_string_similarity/count_string_similarity_by_recursion.c
+++ beauty_of_programming/count_string_similarity/count_string_similarity_by_recursion.c
@@ -14,11 +14,25 @@
 #ifndef MIN
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #endif
+
 typedef int TYPE;
 
+/**
+ * @brief calculate string distance by recursion.
+ *
+ * @param[in]      str_a    string a
+ * @param[in]      a_begin  begin index of a(included)
+ * @param[in]      a_end    end index of a(included)
+ * @param[in]      str_b    string b
+ * @param[in]      b_begin  begin index of b(included)
+ * @param[in]      b_end    end index of b(included)
+ *
+ * @return distance between string a and b
+ */
 TYPE calculate_string_distance(char* str_a, TYPE a_begin, TYPE a_end,
         char* str_b, TYPE b_begin, TYPE b_end)
 {
+    /// check if index of string a or b exceeds their length.
     if(a_begin > a_end) {
         if(b_begin > b_end)
             return 0;
@@ -31,10 +45,12 @@ TYPE calculate_string_distance(char* str_a, TYPE a_begin, TYPE a_end,
         else
             return a_end - a_begin + 1;
     }
+    /// check if the begin value by index of a and b are equal
     if(str_a[a_begin] == str_b[b_begin]) {
         return calculate_string_distance(str_a, a_begin + 1, a_end,
                 str_b, b_begin + 1, b_end);
     } else {
+        /// get the minimum distance for sub-string by three methods.
         TYPE d_1 = calculate_string_distance(str_a, a_begin + 1, a_end,
                 str_b, b_begin, b_end);
         TYPE d_2 = calculate_string_distance(str_a, a_begin, a_end,
@@ -45,12 +61,21 @@ TYPE calculate_string_distance(char* str_a, TYPE a_begin, TYPE a_end,
     }
 }
 
+TYPE calculate_string_distance_by_recursion(char* str_a, TYPE a_length,
+        char* str_b, TYPE b_length)
+{
+    return calculate_string_distance(str_a, 0, a_length - 1,
+            str_b, 0, b_length - 1);
+}
+
 int main(void)
 {
-    char* string_a = "dsfetrtegh";
-    char* string_b = "sdfgerwefe";
-    TYPE distance = calculate_string_distance(string_a, 0, strlen(string_a),
-            string_b, 0, strlen(string_b));
+    char* string_a = "d";
+    char* string_b = "s";
+    TYPE distance = calculate_string_distance_by_recursion(string_a,
+            strlen(string_a),
+            string_b,
+            strlen(string_b));
     printf("string %s and string %s\ndistance: %d similarity: %f\n",
             string_a, string_b, distance, 1 / ((double)distance + 1));
     return EXIT_SUCCESS;
