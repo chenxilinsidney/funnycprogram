8d6f873997d2e31e44ecebcad355db491a2bcf19
Sidney Chen <chenxilinsidney@gmail.com>
2015-01-12 18:53:09 +0800

    program: search max and min value v1

---
+++ beauty_of_programming/search_max_and_min_value/search_max_and_min_value_v1.c
@@ -0,0 +1,64 @@
+/**
+ * @file search_max_and_min_value_v1.c
+ * @brief search the maximum and minimum value in the sequence.
+ * @author chenxilinsidney
+ * @version 1.0
+ * @date 2015-01-12
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+// #define NDEBUG
+#include <assert.h>
+
+// #define NDBG_PRINT
+#include "debug_print.h"
+
+typedef int TYPE;
+
+#define MAX_COUNT      10000000
+TYPE array[MAX_COUNT] = {0};
+
+/**
+ * @brief search the maximum and minimum value in the array
+ *
+ * @param[in]      array  sequence for search
+ * @param[in]      count  sequence length
+ * @param[out]     max    maximum value from the sequence
+ * @param[out]     min    minimum value from the sequence
+ */
+void search_max_and_min_value(TYPE* array, TYPE count, TYPE* max, TYPE* min)
+{
+    assert(max != NULL && min != NULL && count >= 1);
+    /// initialize max and min value
+    *max = INT_MIN;
+    *min = INT_MAX;
+    TYPE i;
+    for (i = 0; i < count; i++) {
+        /// compare with max value for N times(N = count).
+        if (array[i] >= *max)
+            *max = array[i];
+        /// compare with min value for N times(N = count).
+        if (array[i] <= *min)
+            *min = array[i];
+    }
+    /// totally compare 2N times(N = count).
+    return;
+}
+
+int main(void) {
+    /// read data to array
+    TYPE count = 0;
+    while(count < MAX_COUNT && scanf("%u\n", array + count) == 1) {
+        ++count;
+    }
+    /// search maximum and minimum value
+    TYPE max = 0;
+    TYPE min = 0;
+    search_max_and_min_value(array, count, &max, &min);
+    /// output result
+    printf("max element is %d\n", max);
+    printf("min element is %d\n", min);
+    return EXIT_SUCCESS;
+}
