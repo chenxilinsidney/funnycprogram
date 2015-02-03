1f7e7089628b9ea038c00a556cd8a4477b8fcf2f
Sidney Chen <chenxilinsidney@gmail.com>
2015-02-03 22:05:40 +0800

    optimize

--- beauty_of_programming/range_overlap_judgement/range_overlap_judgement.c
+++ beauty_of_programming/range_overlap_judgement/range_overlap_judgement.c
@@ -37,38 +37,66 @@ TYPE range_overlap_judgement(TYPE* source, TYPE* target, TYPE count)
     TYPE i, j;
     for (i = 0; i < count; i++) {
         assert(target[i << 1] <= target[(i << 1) + 1]);
+    }
+    // target range combined
+    TYPE x1, y1, x2, y2;
+    TYPE* flag_targe_done = SCALLOC(count, TYPE);
+    /// get first range
+    for (i = 0; i < count; i++) {
+        if (flag_targe_done[i])
+            continue;
         DEBUG_PRINT_VALUE("%d", i);
+        x1 = target[i << 1];
+        y1 = target[(i << 1) + 1];
+        DEBUG_PRINT_VALUE("%d", x1);
+        DEBUG_PRINT_VALUE("%d", y1);
+        /// do with the second range
+        for (j = i + 1; j < count; j++) {
+            if (flag_targe_done[j])
+                continue;
+            DEBUG_PRINT_VALUE("%d", j);
+            x2 = target[j << 1];
+            y2 = target[(j << 1) + 1];
+            DEBUG_PRINT_VALUE("%d", x2);
+            DEBUG_PRINT_VALUE("%d", y2);
+            /// process two relative range
+            if (x2 >= x1 && x2 <= y1) {
+                if (y1 <= y2) target[(i << 1) + 1] = y2;
+                flag_targe_done[j] = 1;
+            } else if (y2 >= x1 && y2 <= y1) {
+                if (x2 <= x1) target[i << 1] = x2;
+                flag_targe_done[j] = 1;
+            } else if(x2 <= x1 && y1 <= y2) {
+                target[i << 1] = x2;
+                target[(i << 1) + 1] = y2;
+                flag_targe_done[j] = 1;
+            }
+            /// refresh x1 and y1 value
+            x1 = target[i << 1];
+            y1 = target[(i << 1) + 1];
+        }
+    }
+    // display the processed range and flag
+    DEBUG_PRINT_STRING("test flag and range.\n");
+    for (i = 0; i < count; i++) {
+        /// ignore useless range
+        if (flag_targe_done[i])
+            continue;
         DEBUG_PRINT_VALUE("%d", target[i << 1]);
         DEBUG_PRINT_VALUE("%d", target[(i << 1) + 1]);
     }
-    // target range combined
-    TYPE x1, y1, x2, y2, flag = 1;
-    TYPE* targe_new = SMALLOC(count << 1, TYPE);
-    TYPE* count_new = count;
-    memcpy(targe_new, target, count << 1);
-    while (flag) {
-        flag = 0;
-        for (i = 0; i < count; i++) {
-            x1 = target[i << 1];
-            y1 = target[(i << 1) + 1];
-            for (j = i + 1; j < count; j++) {
-                x2 = target[i << 1];
-                y2 = target[(i << 1) + 1];
-                if (x2 >= x1 && x2 <= y1) {
-                    if (y1 <= y2) y1 = y2;
-                    x1 = x2;
-                    count_new--;
-                    flag = 1;
-                } else if (y2 >= x1 && y2 <= y1) {
-                    if (x2 <= x1) x1 = x2;
-                    y2 = y1;
-                    count_new--;
-                    flag = 1;
-                }
-            }
-        }
+    // source range detect
+    DEBUG_PRINT_STRING("test source range.\n");
+    for (i = 0; i < count; i++) {
+        /// ignore useless range
+        if (flag_targe_done[i])
+            continue;
+        /// test if source range in the target range
+        if (target[i << 1] <= source[0] &&
+                target[(i << 1) + 1] >= source[1])
+            return 1;
     }
-    return 1;
+    return 0;
 }
 
 int main(void) {
@@ -85,13 +113,6 @@ int main(void) {
     source[1] = array[count][1];
     printf("the source range(ex:3 5) is [%d,%d]\n",source[0], source[1]);
     printf("the target range count = %d\n", count);
-    DEBUG_PRINT_VALUE("%d", array[0][0]);
-    DEBUG_PRINT_VALUE("%d", array[0][1]);
-    DEBUG_PRINT_VALUE("%p", array);
-    DEBUG_PRINT_VALUE("%p", *array);
-    DEBUG_PRINT_VALUE("%p", *(array + 1));
-    DEBUG_PRINT_VALUE("%p", *(array + 2));
-    DEBUG_PRINT_VALUE("%p", *(array + 1) + 1);
     /// output result
     if(range_overlap_judgement(source, (TYPE*)array, count)) {
         printf("The source range is in the target range.\n");
