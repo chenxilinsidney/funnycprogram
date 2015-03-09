6de6ef9a2cc518a56ca6bc85c4fc9e336b33ddb7
Sidney Chen <chenxilinsidney@gmail.com>
2015-03-09 17:19:47 +0800

    learning sqlite

---
+++ database/sqlite/test.sql
@@ -0,0 +1,16 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE company(
+  ID      INT  PRIMARY KEY  NOT NULL,
+  NAME    TEXT              NOT NULL,
+  AGE     INT               NOT NULL,
+  ADDRESS CHAR(50),
+  SALARY  REAL
+);
+INSERT INTO "company" VALUES(1,'Paul',32,'California',20000.00);
+INSERT INTO "company" VALUES(2,'Allen',25,'Texas',15000.00);
+INSERT INTO "company" VALUES(3,'Teddy',23,'Norway',20000.00);
+INSERT INTO "company" VALUES(4,'Mark',25,'Rich-Mond',65000.00);
+INSERT INTO "company" VALUES(5,'David',27,'Texas',85000.00);
+INSERT INTO "company" VALUES(6,'Kim',22,'South-Hall',45000.00);
+COMMIT;
