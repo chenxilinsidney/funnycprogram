180c68d0c7fb88773c9853ad379c0b1f65170100
Sidney Chen <chenxilinsidney@gmail.com>
2015-03-12 10:20:38 +0800

    exercise 1-7&1-8

--- SICP/sqrt.scm
+++ SICP/sqrt.scm
@@ -1,14 +1,17 @@
-(define (square x)
-  (* x x))
-
 (define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+  (if (good-enough2? guess x)
       guess
       (sqrt-iter (improve guess x)
                  x)))
 
 (define (good-enough? guess x)
   (< (abs (- (square guess) x)) 0.0001))
+
+(define (good-enough2? guess x)
+  (< (/ (abs (- (improve guess x) guess)) guess) 0.0001))
+
+(define (square x)
+  (* x x))
 
 (define (abs x)
   (if (< x 0) (- x) x))
@@ -21,3 +24,22 @@
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
+
+
+
+(define (cube-root-iter guess x)
+  (if (good-enough-cube? guess x)
+      guess
+      (cube-root-iter (improve-cube guess x)
+                 x)))
+
+(define (cube-root x)
+  (cube-root-iter 1.0 x))
+
+(define (good-enough-cube? guess x)
+  (< (/ (abs (- (improve-cube guess x) guess)) guess) 0.0001))
+
+(define (improve-cube guess x)
+  (/ (+ (/ x (square guess))
+        (* 2 guess))
+     3))
