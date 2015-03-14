dbdfeaf75ed91cd7fcc8ca4644eb7969a72ecb93
Sidney Chen <chenxilinsidney@gmail.com>
2015-03-14 19:20:52 +0800

    fast algorithm for expt

--- SICP/expt.scm
+++ SICP/expt.scm
@@ -8,3 +8,15 @@
   (if (= 0 n)
       a
       (expt-v2-iter (* a b) b (- n 1))))
+
+(define (fast-expt-v1 b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt-v1 b (/ n 2))))
+        (else (* b (fast-expt-v1 b (- n 1))))))
+
+(define (fast-expt-v2 b n)
+  (fast-expt-v2-iter 1 b n))
+(define (fast-expt-v2-iter a b n)
+  (cond ((= n 0) a)
+        ((even? n) (fast-expt-v2-iter a (square b) (/ n 2)))
+        (else (fast-expt-v2-iter (* a b) b (- n 1)))))
