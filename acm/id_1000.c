9f06ed02ee42eb3ea7e2bb1874abd6563bfa2898
Sidney Chen <chenxilinsidney@gmail.com>
2015-03-18 10:44:47 +0800

    HDOJ ID 1000

---
+++ acm/id_1000.c
@@ -0,0 +1,18 @@
+/**
+ * @file id_1000.c
+ * @brief HDOJ Problem ID 1000.
+ * @author chenxilinsidney
+ * @version 1.0
+ * @date 2015-03-18
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    while (scanf("%d%d", &a, &b) != EOF) {
+        printf("%d\n",a + b);
+    }
+    return 0;
+}
