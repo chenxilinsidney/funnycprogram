fb71b7485dcff867d7f40716ca21e403e039dae8
Sidney Chen <chenxilinsidney@gmail.com>
2015-03-18 10:56:30 +0800

    HDOJ ID1001 wrong answer

---
+++ acm/id_1001.c
@@ -0,0 +1,18 @@
+/**
+ * @file id_1001.c
+ * @brief HDOJ Problem ID 1001.
+ * @author chenxilinsidney
+ * @version 1.0
+ * @date 2015-03-18
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    while (scanf("%d", &a) != EOF) {
+        printf("%d\n\n", ((a + 1) * a) / 2);
+    }
+    return 0;
+}
