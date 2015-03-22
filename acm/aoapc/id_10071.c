2b63080b9f9d7cb7ba751ac70ede58d26349d2ef
Sidney Chen <chenxilinsidney@gmail.com>
2015-03-22 23:38:32 +0800

    OVAOJ ID 10071

---
+++ acm/aoapc/id_10071.c
@@ -0,0 +1,18 @@
+/**
+ * @file id_10071.c
+ * @brief AOAPC I 10071
+ * @author chenxilinsidney
+ * @version 1.0
+ * @date 2015-03-22
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    while (scanf("%d%d", &a, &b) != EOF) {
+        printf("%d\n", 2 * a * b);
+    }
+    return 0;
+}
