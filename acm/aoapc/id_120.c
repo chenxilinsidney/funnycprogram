404237ef8d0cde3a5ce3f43d6c6229524d9563fc
Sidney Chen <chenxilinsidney@gmail.com>
2015-03-27 00:54:15 +0800

    UVAOJ ID 120 misunderstand the problem should sort all numbers

--- acm/aoapc/id_120.c
+++ acm/aoapc/id_120.c
@@ -14,66 +14,58 @@
 char line[MAX_LINE_LENGTH];
 int pancake[30];
 
+void flip(int* array, int array_index)
+{
+    int index_begin, temp;
+    for (index_begin = 0; index_begin < array_index;
+            array_index--, index_begin++) {
+        temp = array[index_begin];
+        array[index_begin] = array[array_index];
+        array[array_index] = temp;
+    }
+}
+
 int main()
 {
-    while (gets(line)) {
+    while (fgets(line, MAX_LINE_LENGTH, stdin)) {
         int line_length = strlen(line);
         int num_pancake = 0;
         int line_index;
-        int flag = 0, index_begin = 0, index_end = 0;
-        int min_pancake_d = 101, min_pancake_index = 0;
-        int max_pancake_d = 0, max_pancake_index = 0;
+        int flag = 0, index_begin = 0;
         for (line_index = 0; line_index <= line_length; line_index++) {
             if (!flag && isdigit(line[line_index])) {
                 index_begin = line_index;
                 flag = !flag;
             }
             if (flag && !isdigit(line[line_index])) {
-                index_end = line_index - 1;
-                int power = 1;
-                int pancake_d = 0;
-                while (index_begin <= index_end) {
-                    pancake_d += (line[index_end--] - '0') * power;
-                    power *= 10;
-                }
-                num_pancake++;
-                if (min_pancake_d > pancake_d) {
-                    min_pancake_d = pancake_d;
-                    min_pancake_index = num_pancake;
-                }
-                if (max_pancake_d < pancake_d) {
-                    max_pancake_d = pancake_d;
-                    max_pancake_index = num_pancake;
-                }
+                int pancake_d;
+                sscanf(line + index_begin, "%d", &pancake_d);
+                pancake[num_pancake++] = pancake_d;
                 flag = !flag;
             }
         }
-        printf("%s\n", line);
-        if (max_pancake_index == num_pancake &&
-                min_pancake_index == 1) {
-            printf("0\n");
-        } else if (max_pancake_index == 1 &&
-                min_pancake_index == num_pancake) {
-            printf("1 0\n");
-        } else if (max_pancake_index > min_pancake_index &&
-                max_pancake_index < num_pancake) {
-            printf("%d 1 %d 0\n", num_pancake - max_pancake_index + 1,
-                    max_pancake_index - min_pancake_index + 1);
-        } else if (max_pancake_index > min_pancake_index &&
-                max_pancake_index == num_pancake) {
-            printf("%d 0\n", num_pancake - min_pancake_index + 1);
-        } else if (max_pancake_index < min_pancake_index &&
-                min_pancake_index < num_pancake &&
-                max_pancake_index > 1) {
-            printf("%d 1 %d 0\n", num_pancake - max_pancake_index + 1,
-                    min_pancake_index);
-        } else if (max_pancake_index < min_pancake_index &&
-                min_pancake_index == num_pancake) {
-            printf("%d 1 0\n", num_pancake - max_pancake_index + 1);
-        } else if (max_pancake_index < min_pancake_index &&
-                max_pancake_index == 1) {
-            printf("1 %d 0\n", num_pancake - min_pancake_index + 1);
+        printf("%s", line);
+        int index_end = num_pancake;
+        while (index_end) {
+            int max_index = 0;
+            int max_pancake = 0;
+            for (index_begin = 0; index_begin < index_end; index_begin++) {
+                if (pancake[index_begin] > max_pancake) {
+                    max_pancake = pancake[index_begin];
+                    max_index = index_begin;
+                }
+            }
+            if (max_index != index_end - 1) {
+                if (max_index != 0) {
+                    flip(pancake, max_index);
+                    printf("%d ", num_pancake - max_index);
+                }
+                flip(pancake, index_end - 1);
+                printf("%d ", num_pancake - index_end + 1);
+            }
+            index_end--;
         }
+        printf("0\n");
     }
     return 0;
 }
