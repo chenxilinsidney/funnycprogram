1f3de7415e4573af7c39e63d30ea063b9af9ca02
Sidney Chen <chenxilinsidney@gmail.com>
2015-03-27 01:34:27 +0800

    UVAOJ ID 755 REgit add .git add .

--- acm/aoapc/id_755.c
+++ acm/aoapc/id_755.c
@@ -11,49 +11,51 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_WORD_WIDTH 10
-char word_list[10005][MAX_WORD_WIDTH];
+#define MAX_WORD_WIDTH 20
+
+#define MAX_LINE_LENGTH 1025
+char line[MAX_LINE_LENGTH];
+
+char number_table[26] = "2223334445556667777888999";
 
 int compare_string(void const* a, void const* b)
 {
     return strcmp((char*)a, (char*)b);
 }
 
-int change_to_number(int a)
-{
-    if (a < 'Q')
-        return (a - 'A') / 3 + 2 + '0';
-    else
-        return (a - 'A' - 1) / 3 + 2 + '0';
-}
-
 int main()
 {
+    char word_list[10005][MAX_WORD_WIDTH];
     int num_case;
-    scanf("%d\n", &num_case);
+    scanf("%d", &num_case);
     while (num_case--) {
         int num_numbers;
-        scanf("%d\n", &num_numbers);
+        scanf("%d", &num_numbers);
         int word_list_index = 0;
         while (word_list_index < num_numbers) {
             int char_index = 0;
             int character;
-            while ((character = getchar()) != '\n') {
+            fgets(line, MAX_LINE_LENGTH, stdin);
+            int line_index = 0;
+            int line_length = strlen(line);
+            if (line_length < 8)
+                continue;
+            while (line_index < line_length && char_index < 8) {
+                character = line[line_index];
                 if (isdigit(character)) {
                     word_list[word_list_index][char_index] = character;
                     char_index++;
                     if (char_index == 3)
                         word_list[word_list_index][char_index++] = '-';
                 } else if (isupper(character)) {
-                    character = change_to_number(character);
+                    character = number_table[character - 'A'];
                     word_list[word_list_index][char_index] = character;
                     char_index++;
                     if (char_index == 3)
                         word_list[word_list_index][char_index++] = '-';
                 }
+                line_index++;
             }
-            if (char_index != 8)
-                printf("char_index = %d\n", char_index);
             word_list[word_list_index][char_index] = '\0';
             word_list_index++;
         }
@@ -80,3 +82,4 @@ int main()
     }
     return 0;
 }
+
