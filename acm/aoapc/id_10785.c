5aebca3efc1306256f81c602edfaf05b7e52388e
Sidney Chen <chenxilinsidney@gmail.com>
2015-03-26 19:20:47 +0800

    UVAOJ ID 10785 remove bugs

--- acm/aoapc/id_10785.c
+++ acm/aoapc/id_10785.c
@@ -13,12 +13,20 @@
 char* vowel_list = "AUEOI";
 char* consonant_list = "JSBKTCLDMVNWFXGPYHQZR";
 
-char line[255];
+char line_vowel[255];
+char line_consonant[255];
 
-int compare_special(void const* a, void const* b)
+int compare_special_a(void const* a, void const* b)
 {
-    char char_a = ((char*)a)[1];
-    char char_b = ((char*)b)[1];
+    int char_a = ((char*)a)[1];
+    int char_b = ((char*)b)[1];
+    return char_a - char_b;
+}
+
+int compare_special_b(void const* a, void const* b)
+{
+    int char_a = ((char*)a)[0];
+    int char_b = ((char*)b)[0];
     return char_a - char_b;
 }
 
@@ -32,6 +40,9 @@ int main()
         printf("Case %d: ", index_case);
         int length = 0;
         scanf("%d", &length);
+#ifndef ONLINE_JUDGE
+        printf("length = %d\n", length);
+#endif
         int count_even = length / 2;
         int count_odd = length - count_even;
         int temp_index = 0;
@@ -39,6 +50,10 @@ int main()
         int consonant[21] = {0};
         while (count_even > 5) {
             consonant[temp_index] = 5;
+#ifndef ONLINE_JUDGE
+            if (temp_index >= 21)
+                printf("error even = %d\n", temp_index);
+#endif
             count_even -= 5;
             temp_index++;
         }
@@ -46,35 +61,49 @@ int main()
         temp_index = 0;
         while (count_odd > 21) {
             vowel[temp_index] = 21;
+#ifndef ONLINE_JUDGE
+            if (temp_index >= 21)
+                printf("error odd = %d\n", temp_index);
+#endif
             count_odd -= 21;
             temp_index++;
         }
         vowel[temp_index] = count_odd;
-        int line_index = 0;
-        for (temp_index = 0; temp_index < 5; temp_index++) {
-            if (vowel[temp_index] != 0) {
-                while (vowel[temp_index]--) {
-                    line[line_index] = vowel_list[temp_index];
-                    line_index += 2;
-                }
-            } else {
-                break;
-            }
-        }
-        line_index = 1;
+        memset(line_consonant, 0, (length + 1) * sizeof(char));
+        int line_index = 1;
         for (temp_index = 0; temp_index < 21; temp_index++) {
             if (consonant[temp_index] != 0) {
                 while (consonant[temp_index]--) {
-                    line[line_index] = consonant_list[temp_index];
+                    line_consonant[line_index] = consonant_list[temp_index];
                     line_index += 2;
                 }
             } else {
                 break;
             }
         }
-        line[length] = '\0';
-        qsort(line, length / 2, sizeof(line[0]) * 2, compare_special);
-        printf("%s\n", line);
+        qsort(line_consonant, length / 2,
+                sizeof(line_consonant[0]) * 2, compare_special_a);
+        memset(line_vowel, 0, (length + 1) * sizeof(char));
+        line_index = 0;
+        for (temp_index = 0; temp_index < 5; temp_index++) {
+            if (vowel[temp_index] != 0) {
+                while (vowel[temp_index]--) {
+                    line_vowel[line_index] = vowel_list[temp_index];
+                    line_index += 2;
+                }
+            } else {
+                break;
+            }
+        }
+        qsort(line_vowel, length - length / 2,
+                sizeof(line_vowel[0]) * 2, compare_special_b);
+        for (line_index = 0; line_index < length; line_index++) {
+            if (line_index & 0x1) 
+                putchar(line_consonant[line_index]);
+            else
+                putchar(line_vowel[line_index]);
+        }
+        putchar('\n');
     }
     return 0;
 }
