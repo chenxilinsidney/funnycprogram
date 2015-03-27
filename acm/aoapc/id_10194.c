5fb3b89ac4acba74326f7cb57788335b9deae74a
Sidney Chen <chenxilinsidney@gmail.com>
2015-03-27 12:27:52 +0800

    UVAOJ ID 10194 OPTIMIZE BY SCANF PARAMATER('[]')

--- acm/aoapc/id_10194.c
+++ acm/aoapc/id_10194.c
@@ -109,46 +109,14 @@ int main()
         while (games_index < games_numbers) {
             /* get games information */
             fgets(line, MAX_LINE_WIDTH, stdin);
-#ifndef ONLINE_JUDGE
-            printf("game:%s\n", line);
-#endif
-            int line_length = strlen(line);
-            if (line[line_length - 1] == '\n') {
-#ifndef ONLINE_JUDGE
-                printf("fgets a \\n\n");
-#endif
-                line[line_length - 1] = '\0';
-                line_length--;
-            }
-            int line_index;
-            int char_special_a_index;
-            int char_special_b_index;
             char team_a[MAX_TEAM_NAME];
             char team_b[MAX_TEAM_NAME];
             int team_a_goals = 0;
             int team_b_goals = 0;
-            int flag = 1;
-            for (line_index = 0; line_index <= line_length; line_index++) {
-                if (line[line_index] == '#') {
-                    if (flag == 1) {
-                        char_special_a_index = line_index;
-                        flag = !flag;
-                    } else {
-                        char_special_b_index = line_index;
-                    }
-                }
-            }
+            sscanf(line, "%[^#]#%d@%d#%[^\n]", team_a, &team_a_goals,
+                    &team_b_goals, team_b);
 #ifndef ONLINE_JUDGE
-            printf("index:%d:%d\n", char_special_a_index,
-                    char_special_b_index);
-#endif
-            memcpy(team_a, line, char_special_a_index + 1);
-            team_a[char_special_a_index] = '\0';
-            memcpy(team_b, line + char_special_b_index + 1,
-                    line_length - char_special_b_index);
-            sscanf(line + char_special_a_index, "#%d@%d#", &team_a_goals,
-                    &team_b_goals);
-#ifndef ONLINE_JUDGE
+            printf("game:%s\n", line);
             printf("%s, %d, %d, %s\n", team_a, team_a_goals,
                     team_b_goals, team_b);
 #endif
