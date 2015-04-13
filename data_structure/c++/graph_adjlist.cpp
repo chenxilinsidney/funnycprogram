209add7736fbfe53f06c3eeb66a4cc71a963c52d
Sidney Chen <chenxilinsidney@gmail.com>
2015-04-13 16:44:57 +0800

    data structure graph dfs bfs

--- data_structure/c++/graph_adjlist.cpp
+++ data_structure/c++/graph_adjlist.cpp
@@ -7,11 +7,15 @@
  */
 
 #include <iostream>
+#include <cstring>
+#include <queue>
+
 using namespace std;
 
 #define MAXVEX 10              // maximum vertex numbers in the graph
-typedef int VertexType;        // vertex data type
+typedef char VertexType;       // vertex data type
 typedef int EdgeType;          // edge weight type
+bool visit[MAXVEX] = {0};
 
 // edge node to record the adjacent vertex node for a vertex node
 typedef struct EdgeNode {
@@ -41,29 +45,83 @@ Graph* CreateGraph()
     cout << "input vertex data one by one:" << endl;
     for (int index = 0; index < G->numVertexes; index++) {
         cin >> G->adj[index].data;
+        cin.get();
         G->adj[index].firstedge = NULL;
     }
     cout << "input edge each by two vertex index(begin from 0):" << endl;
     for (int index = 0; index < G->numEdges; index++) {
-        int i, j;
-        cin >> i >> j;
+        int i, j, w;
+        cin >> i >> j >> w;
+        cout << i << j << w << endl;
         EdgeNode* e = new EdgeNode;
         e->adjvex = j;
+        e->weight = w;
         e->next = G->adj[i].firstedge;
         G->adj[i].firstedge = e;
 #define NODRECTIONGRAPH
 #ifdef NODRECTIONGRAPH
         e= new EdgeNode;
         e->adjvex = i;
+        e->weight = w;
         e->next = G->adj[j].firstedge;
         G->adj[j].firstedge = e;
 #endif
+    }
+    return G;
+}
+
+void BFS(Graph* G) {
+    memset(visit, 0, sizeof(visit[0]) * MAXVEX);
+    queue<EdgeNode*> q;
+    EdgeNode* temp;
+    for (int index = 0; index < G->numVertexes; index++) {
+        if (!visit[index]) {
+            visit[index] = true;
+            q.push(G->adj[index].firstedge);
+            cout << " from vertex data: " <<
+                G->adj[index].data << endl;
+            while (!q.empty()) {
+                temp = q.front();
+                if (temp && !visit[temp->adjvex]) {
+                    cout << "get weight: " << temp->weight <<
+                        " to " << temp->adjvex << endl;
+                    visit[temp->adjvex] = true;
+                    q.push(temp->next);
+                }
+                q.pop();
+            }
+        }
+    }
+}
+
+void DFS_VISIT(Graph* G, int index)
+{
+    visit[index] = true;
+    cout << "vertex data: " << G->adj[index].data << endl;
+    EdgeNode* temp = G->adj[index].firstedge;
+    while (temp) {
+        if (!visit[temp->adjvex])
+            DFS_VISIT(G, temp->adjvex);
+        temp = temp->next;
+    }
+}
+
+void DFS(Graph* G)
+{
+    memset(visit, 0, sizeof(visit[0]) * MAXVEX);
+    for (int index_i = 0; index_i < G->numVertexes; index_i++) {
+        if (!visit[index_i])
+            DFS_VISIT(G, index_i);
     }
 }
 
 int main(void)
 {
     Graph* G = CreateGraph();
+    cout << "BFS" << endl;
+    BFS(G);
+    cout << "DFS" << endl;
+    DFS(G);
     return 0;
 }
 
--- data_structure/c++/graph_adjmatrix.cpp
+++ data_structure/c++/graph_adjmatrix.cpp
@@ -7,12 +7,15 @@
  */
 
 #include <iostream>
+#include <cstring>
+#include <queue>
 using namespace std;
 
 #define MAXVEX 10              // maximum vertex numbers in the graph
-typedef int VertexType;        // vertex data type
+typedef char VertexType;       // vertex data type
 typedef int EdgeType;          // edge weight type
 #define INFINITY 65535         // set as infinity value
+bool visit[MAXVEX] = {0};
 
 // graph
 typedef struct {
@@ -30,6 +33,7 @@ Graph* CreateGraph()
     cout << "input vertex data one by one:" << endl;
     for (int index = 0; index < G->numVertexes; index++) {
         cin >> G->vertex[index];
+        cin.get();
     }
     // initial adjacent matrix
     for (int index_i = 0; index_i < G->numVertexes; index_i++)
@@ -45,11 +49,61 @@ Graph* CreateGraph()
         G->arc[j][i] = w;
 #endif
     }
+    return G;
+}
+
+void BFS(Graph* G) {
+    memset(visit, 0, sizeof(visit[0]) * MAXVEX);
+    queue<int> q;
+    for (int index = 0; index < G->numVertexes; index++) {
+        if (!visit[index]) {
+            visit[index] = true;
+            q.push(index);
+            while (!q.empty()) {
+                int index_i = q.front();
+                cout << " from vertex " << index_i << " data: " <<
+                    G->vertex[index_i] << endl;
+                for (int index_j = 0; index_j < G->numVertexes; index_j++) {
+                    if (G->arc[index_i][index_j] != INFINITY &&
+                            !visit[index_j]) {
+                        visit[index_j] = true;
+                        cout << "get weight: " << G->arc[index_i][index_j] <<
+                            " to " << index_j << endl;
+                        q.push(index_j);
+                    }
+                }
+                q.pop();
+            }
+        }
+    }
+}
+
+void DFS_VISIT(Graph* G, int index)
+{
+    visit[index] = true;
+    cout << "vertex data: " << G->vertex[index] << endl;
+    for (int index_j = 0; index_j < G->numVertexes; index_j++) {
+        if (G->arc[index][index_j] != INFINITY && !visit[index_j])
+            DFS_VISIT(G, index_j);
+    }
+}
+
+void DFS(Graph* G)
+{
+    memset(visit, 0, sizeof(visit[0]) * MAXVEX);
+    for (int index_i = 0; index_i < G->numVertexes; index_i++) {
+        if (!visit[index_i])
+            DFS_VISIT(G, index_i);
+    }
 }
 
 int main(void)
 {
     Graph* G = CreateGraph();
+    cout << "BFS" << endl;
+    BFS(G);
+    cout << "DFS" << endl;
+    DFS(G);
     return 0;
 }
 
